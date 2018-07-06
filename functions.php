6af1ae635e0202f2c781acb2b0c0cfdd4db4f240
Auxiliar Docente em Informática <e026ti@cps.sp.gov.br>
2018-07-06 10:00:48 -0300

    Adicionado suporte à Title Tag ao tema

--- functions.php
+++ functions.php
@@ -2,5 +2,6 @@
 
 if (function_exists('add_theme_support')) {
     add_theme_support('post-thumbnails');
-    add_theme_support( 'automatic-feed-links' );
+    add_theme_support('automatic-feed-links');
+    add_theme_support('title-tag');
 }
